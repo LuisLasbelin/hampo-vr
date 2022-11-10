a61f52696ac2cafaf3af5bca1300685841e2b876
Llambies <admallla@epsg.upv.es>
2022-11-10 20:36:21 +0100

    Secret commit 1

---
+++ HampoMoon/Assets/Scripts/Seguir_camino.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using PathCreation;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.UI;
+using UnityEngine.UI;
+using Vuforia;
+using Image = UnityEngine.UI.Image;
+
+public class Seguir_camino : MonoBehaviour
+{
+    public PathCreator pathCreator;
+
+    public float velocidad;
+    public float velocidad_maxima;
+    public float aceleracion;
+
+    public float estabilidad;
+    public float estabilidad_maxima;
+
+    public Animator rueda_derecha;
+    public Animator rueda_iquierda;
+
+    public Image imagen;
+
+    private float distancia_recorrida;
+
+    private bool estable;
+    public Button boton_acelerar;
+    public bool acelerando;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {       
+        //Make the Unity Action also call your second function
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distancia_recorrida += velocidad * Time.deltaTime;
+        Quaternion posicion_anterior = transform.rotation;
+        transform.position = pathCreator.path.GetPointAtDistance(-distancia_recorrida);
+        transform.rotation = pathCreator.path.GetRotationAtDistance(-distancia_recorrida);
+
+        float rotacion_relativa = transform.rotation.y - posicion_anterior.y;
+        float rotacion_alterada = Mathf.Abs(rotacion_relativa * 80);
+
+        if (rotacion_alterada > 1 && rotacion_alterada < 50)
+        {
+            Debug.Log(rotacion_alterada);
+            rueda_derecha.SetTrigger("Derecha");
+            rueda_iquierda.SetTrigger("Derecha");
+            if (estabilidad - rotacion_alterada < 0)
+            {
+                estable = false;
+                //imagen.color = Color.red;
+            }
+
+            estabilidad -= rotacion_alterada;
+        }
+        else
+        {
+            if (estabilidad + 1 * Time.deltaTime > 100)
+            {
+                estable = true;
+                //imagen.color = Color.green;
+            }
+            else
+            {
+                estabilidad += 10 * Time.deltaTime;
+            }
+        }
+
+
+        
+        if (acelerando && estable)
+        {
+            if (velocidad + aceleracion * Time.deltaTime <= velocidad_maxima)
+            {
+                velocidad += aceleracion * Time.deltaTime;
+            }
+            else
+            {
+                velocidad = velocidad_maxima;
+            }
+        }else
+        {
+            if (velocidad - aceleracion * 5 * Time.deltaTime >= 0)
+            {
+                velocidad -= aceleracion * 5 * Time.deltaTime;
+            }
+            else
+            {
+                velocidad = 0;
+            }
+        }
+        
+        
+        if (Input.GetKey("a") && !estable)
+        {
+            estabilidad += 5;
+        }
+
+        //imagen.fillAmount = estabilidad / 100;
+    }
+
+    public  void Toggle_acelerar(bool acc)
+    {
+        acelerando = acc;
+        Debug.Log("shit");
+    }
+    
+}
