32951b349a61ddd964cfdd10aaa1653c7521fb55
Luis Belloch <luis.bellochmartinez@gmail.com>
2022-11-15 18:58:42 +0100

    207 - Cuenta atrás inicio del juego fix

---
+++ HampoMoon/Assets/Scripts/CountdownControl.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownControl : MonoBehaviour
+{
+    public float timeBetween = 0.75f;
+    public int number = 3;
+    public bool counting = false;
+    float currTime = 0;
+    int currNumber = 0;
+
+    public Sprite[] numSprites;
+    Image numImage;
+    public string numImageUiName = "NumImage";
+
+
+    private void Start()
+    {
+        counting = false;
+        currTime = timeBetween;
+        currNumber = number;
+
+        numImage = GameObject.Find(numImageUiName).GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+
+        if (GameControl.instance.gameState == GameState.Countdown)
+        {
+            counting = true;
+        }
+
+        if (counting)
+        {
+            if (currTime > 0) currTime -= Time.deltaTime;
+            else
+            {
+                currTime = timeBetween;
+                currNumber -= 1;
+                numImage.sprite = numSprites[currNumber];
+                Debug.Log("Count: " + currNumber);
+            }
+        }
+
+        if (counting && currNumber <= 0)
+        {
+            GameControl.instance.UpdateGameState(GameState.Race);
+            counting = false;
+        }
+    }
+}
