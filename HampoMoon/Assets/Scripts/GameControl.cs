1cb1a6ceb7c55108e048bd4048553f0fa9f70ab2
Luis Belloch <luis.bellochmartinez@gmail.com>
2022-11-15 11:59:38 +0100

    205 - Interfaz de carrera

--- HampoMoon/Assets/Scripts/GameControl.cs
+++ HampoMoon/Assets/Scripts/GameControl.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameControl : MonoBehaviour
 {
     public static GameControl instance;
     public GameState gameState;
-
+    public string gameUiScene = "GameUI";
+    public string currLapName = "CurrLap";
+    Image currLapImage;
+    public string maxLapsName = "MaxLaps";
+    Image maxLapsImage;
+    public Sprite[] lapsNums;
 
     public int laps = 0;
     public int maxLaps = 5;
@@ -15,11 +22,23 @@ public class GameControl : MonoBehaviour
     {
         // Asegura que solo hay 1
         instance = this;
+        SceneManager.LoadScene(gameUiScene, LoadSceneMode.Additive);
     }
-    
+
+    private void Start()
+    {
+        currLapImage = GameObject.Find(currLapName).GetComponent<Image>();
+        currLapImage.sprite = lapsNums[0];
+
+        maxLapsImage = GameObject.Find(maxLapsName).GetComponent<Image>();
+        maxLapsImage.sprite = lapsNums[maxLaps];
+    }
+
     public void NewLap()
     {
         laps++;
+
+        currLapImage.sprite = lapsNums[laps];
 
         // Al llegar al maximo de vueltas se termina la carrera
         if (laps >= maxLaps) UpdateGameState(GameState.EndRace);
