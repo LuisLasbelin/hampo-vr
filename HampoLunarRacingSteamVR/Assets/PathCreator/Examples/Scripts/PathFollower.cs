0378a765ab1a5e4e07dfc99103820282713425c9
Luis Belloch <luis.bellochmartinez@gmail.com>
2022-12-09 16:27:26 +0100

    Proyecto nuevo para Steam VR

---
+++ HampoLunarRacingSteamVR/Assets/PathCreator/Examples/Scripts/PathFollower.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace PathCreation.Examples
+{
+    // Moves along a path at constant speed.
+    // Depending on the end of path instruction, will either loop, reverse, or stop at the end of the path.
+    public class PathFollower : MonoBehaviour
+    {
+        public PathCreator pathCreator;
+        public EndOfPathInstruction endOfPathInstruction;
+        public float accel = 2;
+        public float speed = 0;
+        public float maxSpeed = 10;
+        float distanceTravelled;
+
+        void Start() {
+            if (pathCreator != null)
+            {
+                // Subscribed to the pathUpdated event so that we're notified if the path changes during the game
+                pathCreator.pathUpdated += OnPathChanged;
+            }
+        }
+
+        void Update()
+        {
+            if (pathCreator != null)
+            {
+                distanceTravelled += speed * Time.deltaTime;
+                speed = Mathf.Clamp(speed - 0.5f, 0, maxSpeed);
+                transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
+                transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
+            }
+        }
+
+        // If the path changes during the game, update the distance travelled so that the follower's position on the new path
+        // is as close as possible to its position on the old path
+        void OnPathChanged() {
+            distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(transform.position);
+        }
+    }
+}
