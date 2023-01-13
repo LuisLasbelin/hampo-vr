f48c408b134ed753e091e2f8de6ce73cca2eff58
Luis Belloch <luis.bellochmartinez@gmail.com>
2023-01-13 13:27:15 +0100

    405-control video 360

--- HampoLunarRacingSteamVR/Assets/Scripts/VideoManager.cs
+++ HampoLunarRacingSteamVR/Assets/Scripts/VideoManager.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Valve.VR;
 
 public class VideoManager : MonoBehaviour
 {
     public string menuScene = "MenuInicio";
+    public SteamVR_Action_Boolean exitAction;
+
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Seleccionar"))
+        if(exitAction.state)
         {
             SceneManager.LoadScene(menuScene, LoadSceneMode.Single);
         }
